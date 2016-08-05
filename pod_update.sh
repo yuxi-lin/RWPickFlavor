3fb7ac695f7794b3159be170946ae1ace07c9322
Yuxi Lin <yuxi.lin@mobileforming.com>
2016-08-05 15:27:00 -0700

    updating and tagging pod to 0.2.13

--- pod_update.sh
+++ pod_update.sh
@@ -15,6 +15,9 @@
     echo "*** Example:  ./script_name 0.0.1 masterspecs ***"
     echo "*** Pod spec repo can be added by:  pod spec repo add <repo name> <repo url> ***"
     echo " "
+    echo "Optional:  can add git commit comment as 3rd parameter"
+    echo "Example:  ./script_name 0.0.1 masterspects \"commit comment\""
+    echo " "
     exit 1
   fi
 
@@ -28,7 +31,7 @@
     exit 1
   fi
 
-  if [ -z "$2" ]
+  if [ ! -z "$2" ]
   then
     podSpecRepo="$2"
   fi
@@ -59,8 +62,8 @@
   fi
 
 # Increment Version Number
-read major minor build <<< $( echo ${currentVersion} | awk -F"." '{print $1" "$2" "$3}' )
-read newMajor newMinor newBuild <<< $( echo ${increment} | awk -F"." '{print $1" "$2" "$3}' )
+  read major minor build <<< $( echo ${currentVersion} | awk -F"." '{print $1" "$2" "$3}' )
+  read newMajor newMinor newBuild <<< $( echo ${increment} | awk -F"." '{print $1" "$2" "$3}' )
 
   if [ "$newMajor" -gt "0" ]
   then
@@ -79,34 +82,42 @@ read newMajor newMinor newBuild <<< $( echo ${increment} | awk -F"." '{print $1"
   echo "new version:  $major.$minor.$build"
   sed -i "" -e  "1,/s.version.*/s/s.version.*/s.version = \'$major.$minor.$build\'/" "$podspecFilename"
   
+# Commit changes to Git
+  commitComment="updating and tagging pod to $major.$minor.$build"
+  if [ ! -z "$3" ]
+  then
+    commitComment="$commitComment - $3"
+  fi
 
-# Commit and push changes
+  echo ' ~~~ commiting and pushing changes to git with comment: $commitComment  ~~~ '
+  echo " "
+  git add .
+  git commit -m "$commitComment"
+  $(git tag "$major"."$minor"."$build")
 
-echo 'commiting and pushing changes to git'
-echo " "
-git add .
-git commit -m "updating and tagging pod to $major.$minor.$build"
-$(git tag "$major"."$minor"."$build")
-git push origin master --tags
+# Validate Podspec
+  echo " "
+  echo ' ~~~ checking podspec ~~~ '
+  echo " "
+  pod lib lint
 
-echo " "
-echo ' ~~~ checking podspec ~~~ '
-echo " "
-pod lib lint
+  if [ $? -ne 0 ]
+  then
+    echo "*** ERROR: podspec did not pass vaidation."
+    echo "if your podspec has public/private dependencies, this may be expected"
+    read -p "Press any key to continue; ctrl+c to exit"
+  fi
 
-if [ $? -ne 0 ]
-then
-  echo "*** ERROR: podspec did not pass vaidation."
-  echo "if your podspec has public/private dependencies, this may be expected"
-  read -p "Press any key to continue; ctrl+c to exit"
-fi
+# Push to Git
+  git push origin master --tags
 
-echo " "
-echo ' ~~~ pushing podspec to spec repo ~~~ '
-echo " "
-echo pod repo push --allow-warnings "$podSpecRepo" "$podspecFilename"
-echo " "
-echo 'Done'
-echo " "
-exit 0
+# Push the Podspec
+  echo " "
+  echo ' ~~~ pushing podspec to spec repo ~~~ '
+  echo " "
+  echo $(pod repo push "$podSpecRepo" "$podspecFilename")
+  echo " "
+  echo 'Done'
+  echo " "
+  exit 0
 
